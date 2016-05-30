5bded6c14abdc19f383c291c62b83df64b9b7657
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-05-30 14:04:23 -0300

    Adição da definição do método carregaDicionario.
    Método incompleto e não testado.

--- Dicionario/src/br/unisinos/Tradutor.java
+++ Dicionario/src/br/unisinos/Tradutor.java
@@ -6,8 +6,22 @@ private AVLTree AVLTreeDicionario;
 
 //Carrega o arquivo de dicionário(dicionário.dat) para a árvore AVL.
 protected void carregaDicionario(String arq){
-  
-  
+  try {
+	BufferedReader in = new BufferedReader(new FileReader("dicionario.dat"));
+	
+	String line = in.readLine();
+	while (line != null){
+		String [] arrayInfo = line.split(" ");
+		arrayInfo [0]
+		
+		line = in.readLine();
+	}
+	
+	
+	} catch (IOException e) {
+		System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+	} 
+	
 }
 
 //Traduz uma única palavra. Este método recebe como parâmetro a palavra a ser traduzida e retorna a lista das possíveis traduções para esta palavra.
