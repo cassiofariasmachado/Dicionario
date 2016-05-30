240726c0d34c5aebec38333f462b72edc7bc85d3
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-05-30 12:28:46 -0300

    Criação da classe Tradutor.java
    Métodos ainda não definidos.

---
+++ Dicionario/src/br/unisinos/Tradutor.java
@@ -0,0 +1,29 @@
+package br.unisinos;
+
+public class Tradutor{
+
+private AVLTree AVLTreeDicionario;
+
+//Carrega o arquivo de dicionário(dicionário.dat) para a árvore AVL.
+protected void carregaDicionario(String arq){
+  
+  
+}
+
+//Traduz uma única palavra. Este método recebe como parâmetro a palavra a ser traduzida e retorna a lista das possíveis traduções para esta palavra.
+public List traduzPalavra(String palavra){
+
+
+}
+
+//Insere uma nova definição no dicionário. Recebe como parâmetro a palavra em inglês e lista de possíveis traduções.
+public void insereTraducao(String palavra, List definicoes) {
+
+}
+
+//Salva o arquivo de dicionário (dicionário.dat) com as respectivas definições baseado no conteúdo da árvore AVL.
+public void salvaDicionario(String arq){
+
+}
+
+}
